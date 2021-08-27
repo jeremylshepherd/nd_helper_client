bfdc0bd6c3d3ba55fecf4cbf9149178b1005dff2
Jeremy L. Shepherd <jeremy.shepherd@lexisnexis.com>
2021-08-27 16:20:19 -0400

    💄 Additional improvements. Started on making Task "updateable".

--- src/components/task/TaskView.tsx
+++ src/components/task/TaskView.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { spawn } from 'child_process';
+import React, { useState } from 'react';
 import { Status } from '../../types';
 import './_component.task.scss';
 
@@ -11,6 +12,8 @@ interface TaskViewProps {
 
 function TaskView(props: TaskViewProps) {
     const { id, task, comments, status } = props;
+    const [currentStatus, setCurrentStatus] = useState(status);
+    const [showUpdate, setShowUpdate] = useState(false);
     const statusIcon = {
         "pending": "redo",
         "inProgress": 'spinner',
@@ -18,6 +21,26 @@ function TaskView(props: TaskViewProps) {
     };
     const getIcon = (obj : any, status: string) : string => obj[status];
     const dateString = new Date(id).toLocaleDateString('en-us', { month: 'long', year: 'numeric', day: '2-digit'});
+    const renderStatusUpdate = () => (
+        <div className="task-alert">
+            <div className="alert alert-success">
+                <strong>Update Task Status!</strong>
+                <form className="form-group">
+                    <div className="cb-group">
+                        {
+                            Object.keys(Status).map(key => (
+                                <div className="cb-row text-left">
+                                    <input type="checkbox" name={key} value={key} />
+                                    <div className="text-left">{key}</div>
+                                </div>
+                            )) 
+                        }
+                    </div>
+                    <button className="btn btn-danger" onClick={e => {e.preventDefault(); setShowUpdate(false)}}>Close</button>
+                </form>            
+            </div>
+        </div>
+    );
 
     return (
         <div className={`task task-${status}`}>
@@ -29,10 +52,11 @@ function TaskView(props: TaskViewProps) {
             <div className="task-footer">
                 <div className="task-footer-date">{dateString}</div>
                 <div className="task-footer-status">
-                    <span className="task-footer-status-text">{status}</span>
+                    <span className="task-footer-status-text" onClick={() => setShowUpdate(true)}>{status}</span>
                     <i className={`fas fa-${getIcon(statusIcon, status)}`}/>
                 </div>
             </div>
+            {showUpdate && renderStatusUpdate() }
         </div>
 
     );
